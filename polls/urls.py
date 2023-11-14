3ca066f46864ebc10841857b64ad194974ec67f6
max <kindly999999@gmail.com>
2023-11-14 05:36:07 +0100

    add product index in url

--- polls/urls.py
+++ polls/urls.py
@@ -3,14 +3,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from .views import (
     product_view,
-    preview_view
+    preview_view,
+    for_redirect
 )
 
 app_name = 'polls'
 
 urlpatterns = [
-    path('product/', product_view, name='product'),
     path('preview/', preview_view, name='preview'),
+    path('product/<int:index>/', product_view, name ='product'),
+    path('product/', for_redirect, name='product_redirect'),
+    
 ]
 
 if settings.DEBUG:
