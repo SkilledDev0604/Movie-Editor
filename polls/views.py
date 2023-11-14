9e73d5c55dc59de8574e3e6d967cfe8c0f915826
max <kindly999999@gmail.com>
2023-11-14 05:38:07 +0100

    add product index number in views

--- polls/views.py
+++ polls/views.py
@@ -8,9 +8,17 @@ from django.conf import settings
 import datetime
 import os
 
-def product_view(request):
+video_count = 1
+functions = (make_video_1,)
+
+def for_redirect(request):
+    return redirect('/video/product/0')
+
+def product_view(request, index):
+    if not 0 <= index < video_count:
+        return redirect('/video/product/0')
+
     if request.method == 'POST':
-        form = ImageForm(request.POST, request.FILES)
         uploaded_image_dir = f"{settings.BASE_DIR}/static/images/uploaded/"
         image_file = request.FILES.get('image')
         image = None
@@ -20,14 +28,14 @@ def product_view(request):
             with open(image, 'wb+') as destination:
                 for chunk in image_file.chunks():
                     destination.write(chunk)
-        preview_url = make_video_1(request.POST, image)
+        preview_url = functions[index](request.POST, image)
         if image and os.path.exists(image):
             os.remove(image)
         query_params = QueryDict(mutable=True)
         query_params['preview_url'] = preview_url
         redirect_url = '/video/preview/?{}'.format(query_params.urlencode())
         return redirect(redirect_url)
-    return render(request, 'polls/product.html', {"form": ImageForm})
+    return render(request, f'polls/{index}.html', {"form": ImageForm})
 
 def preview_view(request):
     preview_url = request.GET.get('preview_url')
